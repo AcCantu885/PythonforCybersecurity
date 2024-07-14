7cfc28b6f62ca9df30419866e230e02368af4468
AcCantu885 <cantu.anthony@student.greenriver.edu>
2024-07-14 14:18:23 -0700

    Added to end of script.

--- Lesson-1.py
+++ Lesson-1.py
@@ -5,5 +5,12 @@ name = input("What is your name: ")
 print(f"Hello, {name}!")
 
 #Print Affirmation
-print("Hi! You're going to have an awesome day!")
+print("Hi! You're going to have an awesome day! Happy Birthday!")
 
+#Prompting users for their age
+age = int(input("What is your age? "))
+
+#We want to take their age and add 2 to it
+new_age = age + 2
+#We will print the added age
+print(new_age)
