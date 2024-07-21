f10ee862904f852b43d118c93664763b7c1281b3
AcCantu885 <cantu.anthony@student.greenriver.edu>
2024-07-21 15:39:22 -0700

    Prompting user with the question Conditional 1

---
+++ Conditional-1.py
@@ -0,0 +1,6 @@
+#Prompt the user with the question "Is today a good day?"
+answer = input("Is today a good day? (y/n): ")
+
+#Checking the user's input and print the appropriate answer
+if answer.lower() == 'y':
+    print("Yes it is")
