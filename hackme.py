103e701eff2edf18c28b11dcc87225fcc5a12019
AcCantu885 <cantu.anthony@student.greenriver.edu>
2024-07-27 16:18:58 -0700

    Updates to creating the files and saving the info

--- hackme.py
+++ hackme.py
@@ -12,3 +12,17 @@ print(f"Favorite color: {favorite_color}")
 print(f"First pet's name: {first_pet}")
 print(f"Mother's maiden name: {mother_maiden_name}")
 print(f"Elementary school: {elementary_school}")
+
+#Collect the information into a string
+info = (
+    f"Name: {name}\n"
+    f"Favorite color: {favorite_color}\n"
+    f"First pet's name: {first_pet}\n"
+    f"Mother's maiden name: {mother_maiden_name}\n"
+    f"Elementary school: {elementary_school}\n"
+)
+
+#We save the information to a file called "hackme.txt"
+with open("hackme.txt", "w") as file:
+    file.write(info)
+print("Information saved to hackme.txt")
