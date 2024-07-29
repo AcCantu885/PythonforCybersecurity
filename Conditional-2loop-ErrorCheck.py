abe58367a6660222787051c5914033fc8b528ec0
AcCantu885 <cantu.anthony@student.greenriver.edu>
2024-07-28 22:03:07 -0700

    Updated the conditional/loop with a function

--- Conditional-2loop-ErrorCheck.py
+++ Conditional-2loop-ErrorCheck.py
@@ -1,3 +1,9 @@
+#Defined the function to print message based on user's input
+def send_message(response):
+   message = "Yes it is" if response == 'y' else "No, it is not"
+   for _ in range(10):
+      print(message)
+
 #Prompt the user with the question "Is today a good day?"
 answer = input("Is today a good day? (y/n): ")
 
